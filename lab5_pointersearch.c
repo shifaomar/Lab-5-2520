c0ad0109871bbe97abb592d66e75a1c628c479eb
Shifa <ssheik08@uoguelph.ca>
2023-10-25 17:00:16 -0400

    final

--- lab5_pointersearch.c
+++ lab5_pointersearch.c
@@ -4,60 +4,39 @@
  * containing print statements indicating
  * the search state as we proceed
  */
+
 #include <stdio.h>
 #include <string.h> // for strcmp()
 
 #include "bsearch-verbose.h"
 #include "FruitData.h"
 
-
 /**
- * Comparator function for generic search based
- * on the latin name in a pointer to pointer
+ * Comparator function for generic search based on the latin name in a pointer to pointer
  */
-static int
-pointerComparator_LatinName(const void *vKey, const void *vData)
+static int pointerComparator_LatinName(const void *vKey, const void *vData)
 {
-	FruitData **fruitData = NULL;
-    char *key = NULL;
-    int result = 0;
+	FruitData **fruitData = (FruitData **)vData;
+	char *key = (char *)vKey;
+	int result;
 
-	printf(" = = pointerComporator_LatinName: vKey = %p, vData = %p\n", vKey, vData);
+	printf(" = = pointerComparator_LatinName: vKey = %p, vData = %p\n", vKey, vData);
 
+	// Compare the key to the "latin" field in the data referenced by the pointer that the *data" passed in here *points at*.
+	// That is, the data in the struct pointed to by the pointer *the data pointer is pointing at*.
 
-	// TO DO:
-	// Get the key and data from the void pointers.  Keep in mind the
-	// data is now a "pointer to pointer"
+	result = strcmp(key, (*fruitData)->latin);
 
+	printf(" = = pointerComparator_LatinName: key = %s\n", key);
+	printf(" = = pointerComparator_LatinName: fruit = %s, %s\n",
+		   (*fruitData)->common, (*fruitData)->latin);
+	printf(" = = pointerComparator_LatinName: comparison of (%s/%s) returns %d\n",
+		   key, (*fruitData)->latin, result);
 
-
-	// TO DO:
-	// Compare the key to the "latin" field in the data referenced
-	// by the pointer that the *data" passed in here *points at*.
-	//
-	// That is, the data in the struct pointed to by the pointer
-	// *the data pointer is pointing at*.
-	//
-	// It probably will help if you first print out the key and
-	// the data fields first, so you know that you have the dereferencing
-	// correct.
-	//
-	// Hint: it may be clearer to you if you dereference the pointer
-	// iyou get (i.e.; with a '*') and store that value before trying
-	// to deference it to get at a field.
-	//
-	// Feel free to add as many variables as you like to track the
-	// pstages of the dereferencing.
-	//
-	// If you are having trouble, consider the memory addresses values
-	// you see in the vKey and vData above.
-
-
-    return result;
+	return result;
 }
 
-void 
-searchInLatinName(FruitData * const *fruitDataPointers, int nFruits, char *key)
+void searchInLatinName(FruitData *const *fruitDataPointers, int nFruits, char *key)
 {
 	FruitData **pointerResult = NULL;
 	int arrayIndex;
@@ -65,25 +44,23 @@ searchInLatinName(FruitData * const *fruitDataPointers, int nFruits, char *key)
 	printf("Latin names in array of %d pointers to structs:\n", nFruits);
 	printDataPointerArray(fruitDataPointers, nFruits);
 
-
 	printf("Demonstration of comparator (comparing key to first data element):\n");
 	printf("Entering binary search.\n");
 	pointerComparator_LatinName(key, fruitDataPointers);
 
-
 	printf("Entering binary search.\n");
 
-	// TO DO:
-	// Call the binary search with the array of pointers to pointers
-	// to the data, the appropriate size, and the comparator above
+	// Call the binary search with the array of pointers to pointers to the data, the appropriate size, and the comparator above
+	pointerResult = binarysearch(key, fruitDataPointers, nFruits, sizeof(FruitData *), pointerComparator_LatinName);
 
-
-	if (pointerResult != NULL) {
+	if (pointerResult != NULL)
+	{
 		arrayIndex = pointerResult - fruitDataPointers;
 		printf("Latin name search using '%s' returned:\n\t", key);
 		printFuitWithID(*pointerResult, arrayIndex);
-	} else {
+	}
+	else
+	{
 		printf("Latin name search using '%s' FAILED\n", key);
 	}
 }
-
